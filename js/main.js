c19498984e3c29898a16dc986b5552d95b8752de
Connant <diconna@gmail.com>
2021-05-31 14:11:40 +0700

    fixed bugs 2-1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
-}
+function getRandomFloat(min, max, fractionalNumbers) {
+  return (Math.random() * (max - min + 1) + min).toFixed(fractionalNumbers);
+};
 
-getRandomFloat(0,10)
-getRandomInt(0, 10)
+getRandomInt(0, 10);
+getRandomFloat(0,10, 5);
 
+// сама не додумалась, гуглить разрешили
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // https://myrusakov.ru/js-random-numbers.html
+// p.s. ничего не понятно, но очень интересно
