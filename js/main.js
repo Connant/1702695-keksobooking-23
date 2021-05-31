52bd58f154885e51d794711e1f0b783ebb025565
Connant <diconna@gmail.com>
2021-05-31 14:30:15 +0700

    fixed bugs 2-1 2.0

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
 function getRandomFloat(min, max, fractionalNumbers) {
   return (Math.random() * (max - min + 1) + min).toFixed(fractionalNumbers);
-};
+}
 
 getRandomInt(0, 10);
 getRandomFloat(0,10, 5);
