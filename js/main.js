79b923fc0a6924a49bb288ced84cfebbc087908e
Connant <diconna@gmail.com>
2021-06-09 18:24:30 +0700

    data generation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const AVATAR = [img/avatars/user01.png, img/avatars/user02.png, img/avatars/user03.png, img/avatars/user04.png, img/avatars/user05.png,
+  img/avatars/user06.png, img/avatars/user07.png, img/avatars/user08.png];
+const TITLE = ['Old palace', 'Spacious apartment', 'Two-storey house', 'Tree house', 'Dollhouse'];
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const DESCRIPTION = ['House with the ghosts', 'Sunny side', 'Large loggia', 'Next to the park'];
+const TIME = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/duonguyen-8LrGtIxxa4w.randomIdxpg',
+  'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.randomIdxpg',
+  'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.randomIdxpg'];
+
+const numberObjects = 10;
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -8,10 +21,38 @@ function getRandomFloat(min, max, fractionalNumbers) {
   return (Math.random() * (max - min + 1) + min).toFixed(fractionalNumbers);
 }
 
-getRandomInt(0, 10);
-getRandomFloat(0,10, 5);
+const getRandomElement = (items) => items[getRandomInt(0, items.length - 1)];
 
-// сама не додумалась, гуглить разрешили
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// https://myrusakov.ru/js-random-numbers.html
-// p.s. ничего не понятно, но очень интересно
+const generateObject = () => {
+  const COORDINATES = {
+    lat: getRandomFloat(30, 40, 6),
+    lng: getRandomFloat(10, 15, 6),
+  };
+
+  return {
+    author: {
+      avatar: getRandomElement(AVATAR),
+    },
+    offer: {
+      title: getRandomElement(TITLES),
+      address: `Coordinates: ${COORDINATES.lat}, ${COORDINATES.lng}`,
+      price: getRandomInt(0, 10000),
+      type: getRandomElement(TYPE),
+      rooms: getRandomInt(1, 5),
+      guests: getRandomInt(1, 5),
+      checkin: getRandomElement(TIME),
+      checkout: getRandomElement(TIME),
+      features: getRandomElement(FEATURES),
+      description: getRandomElement(DESCRIPTION),
+      photos: getRandomElement(PHOTOS),
+      location: {
+        lat: COORDINATES.lat,
+        lng: COORDINATES.lng,
+      },
+    },
+  };
+};
+
+const similarOffers = new Array(numberObjects).fill(null).map(() => creatOffers());
+
+similarOffers();
