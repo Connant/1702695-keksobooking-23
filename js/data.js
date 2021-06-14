d6efc267f127f15481e0fb0837ce4265bb8c6b83
Connant <diconna@gmail.com>
2021-06-14 12:32:19 +0700

    fixed bugs x2

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-import {getRandomInt, getRandomFloat, getRandomElement} from './utils';
+import {getRandomInt, getRandomFloat, getRandomElement} from './utils.js';
 
 const AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png',
   'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png', 'img/avatars/user09.png', 'img/avatars/user10.png'];
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {similarOffers} from './data';
+import {similarOffers} from './data.js';
 
 // eslint-disable-next-line no-console
 console.log(similarOffers());
