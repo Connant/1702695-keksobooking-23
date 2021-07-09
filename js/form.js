754a736620848f566b86175e28daaccc68176844
Connant <diconna@gmail.com>
2021-07-09 17:54:09 +0700

    validate form step one

--- js/form.js
+++ js/form.js
@@ -5,6 +5,60 @@ const mapForm = document.querySelector('.map__filters');
 const mapSelects = mapForm.querySelectorAll('select');
 const mapFeatures = mapForm.querySelector('.map__features');
 
+const minTitleLength = 30;
+const maxTitleLength = 100;
+
+const adTitle = adForm.querySelector('#title');
+const roomsSelect = adForm.querySelector('#room_number');
+const guestsSelect = adForm.querySelector('#capacity');
+
+const rooms = {
+  one: '1',
+  two: '2',
+  three: '3',
+  oneHundreed: '100',
+};
+
+const guests = {
+  one: '1',
+  two: '2',
+  three: '3',
+};
+
+const checkRoomsAvailable = () => {
+  const numberOfRooms = roomsSelect.value;
+  const numberOfguests = guestsSelect.value;
+  let error = '';
+
+  if (numberOfRooms === rooms.one && numberOfguests !== guests.one) {
+    error += 'Для 1 гостя';
+  } else if (numberOfRooms === rooms.two && numberOfguests !== guests.two) {
+    error += 'Для 2 гостей';
+  } else if (numberOfRooms === rooms.three && numberOfguests !== guests.three) {
+    error += 'Для 3 гостей';
+  } else if (numberOfRooms === rooms.oneHundreed) {
+    error += 'Не для гостей';
+  }
+  guestsSelect.setCustomValidity(error);
+  guestsSelect.reportValidity();
+};
+
+const checkTitleLength = () => {
+  const valueLength = adTitle.value.length;
+
+  if (valueLength < minTitleLength) {
+    adTitle.setCustomValidity(`Добавьте еще ${minTitleLength - valueLength} символа`);
+  } else if (valueLength > maxTitleLength) {
+    adTitle.setCustomValidity(`Удалите лишние ${valueLength - maxTitleLength} символа`);
+  } else {
+    adTitle.setCustomValidity('');
+  }
+  adTitle.reportValidity();
+};
+
+adTitle.addEventListener('input', checkTitleLength);
+adForm.addEventListener('change', checkRoomsAvailable);
+
 export const disableFilters = () => {
   mapForm.classList.add('map__filters--disabled');
   mapSelects.forEach((select) => {
